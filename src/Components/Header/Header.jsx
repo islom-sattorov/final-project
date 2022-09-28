bd1b9b6ac57072f2935e0eb8f13dcdbfda5e2f5d
script <slmsttrv1@gmail.com>
2022-09-28 12:56:33 +0500

    modal template

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,3 +1,6 @@
+import Box from '@mui/material/Box'
+import Modal from '@mui/material/Modal'
+import Typography from '@mui/material/Typography'
 import { useDispatch, useSelector } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 import { Link } from 'react-scroll'
@@ -10,10 +13,21 @@ import pinterest from './pinterest.svg'
 import twitter from './twitter.svg'
 
 
+const boxStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+
 const Header = () => {
     const login = useSelector((state) => state.login.loginStatus);
     const dispatch = useDispatch();
-
     return (
         <>
             <header className={style.header}>
@@ -55,6 +69,20 @@ const Header = () => {
                     </div>
                 </div>
             </nav>
+            <Modal
+                // open={login}
+                // onClose={open}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description">
+                <Box sx={boxStyle}>
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                        Text in a modal
+                    </Typography>
+                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                    </Typography>
+                </Box>
+            </Modal>
         </>
     )
 }
