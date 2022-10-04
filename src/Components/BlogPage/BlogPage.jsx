812d27ba98ba03319353db706ea0fee7284a4304
script <slmsttrv1@gmail.com>
2022-10-04 09:11:09 +0500

    fix: deploy error

--- src/Components/BlogPage/BlogPage.jsx
+++ src/Components/BlogPage/BlogPage.jsx
@@ -1,4 +1,4 @@
-import Box from '@mui/material/Box';
+import { Box } from '@mui/material';
 import MenuItem from '@mui/material/MenuItem';
 import Modal from '@mui/material/Modal';
 import TextField from '@mui/material/TextField';
