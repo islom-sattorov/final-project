1063c7087bb91a1c65fe62429015aa961afe599a
script <slmsttrv1@gmail.com>
2022-10-10 08:27:28 +0500

    add: useLocation() router, conditional about us

--- src/Components/Header/BurgerNav.jsx
+++ src/Components/Header/BurgerNav.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import { Link } from "react-scroll";
 import style from './Header.module.scss';
 
 const BurgerNav = () => {
     const [isActive, setIsActive] = useState(false);
+    const location = useLocation();
 
     return (
         <button
@@ -14,12 +15,13 @@ const BurgerNav = () => {
             <span className="hamburger-box">
                 <div className={isActive ? style.burger_flex : style.burger_hide}>
                     <NavLink className={({ isActive }) => (isActive ? style.navbar_item_active : style.navbar_item)} to='/'><li>Home</li></NavLink>
-                    <Link to='about'
-                        spy={true}
-                        smooth={true}
-                        duration={700}
-                        className={style.navbar_item}>
-                        <li>About Us</li></Link>
+                    {location.pathname === '/' ?
+                        <Link to='about'
+                            spy={true}
+                            smooth={true}
+                            duration={700}
+                            className={style.navbar_item}>
+                            <li>About Us</li></Link> : <></>}
                     <NavLink className={({ isActive }) => (isActive ? style.navbar_item_active : style.navbar_item)} to='/menu'><li>Our Menu</li></NavLink>
                     <NavLink className={({ isActive }) => (isActive ? style.navbar_item_active : style.navbar_item)} to='/reservation'><li>Reservation</li></NavLink>
                     <NavLink className={({ isActive }) => (isActive ? style.navbar_item_active : style.navbar_item)} to='/blog'><li>Blog</li></NavLink>
--- src/Components/Header/HeaderNavBar.jsx
+++ src/Components/Header/HeaderNavBar.jsx
@@ -1,22 +1,24 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import { Link } from 'react-scroll';
 import BurgerNav from './BurgerNav';
 import style from './Header.module.scss';
 import HeaderIcons from './HeaderIcons';
 
 const HeaderNavBar = () => {
+    const location = useLocation()
     return (
         <nav className={style.navbar}>
             <div className='container'>
                 <nav className={style.navbar_flex}>
                     <ul className={style.navbar_items}>
                         <NavLink className={({ isActive }) => (isActive ? style.navbar_item_active : style.navbar_item)} to='/'><li>Home</li></NavLink>
-                        <Link to='about'
-                            spy={true}
-                            smooth={true}
-                            duration={700}
-                            className={style.navbar_item}>
-                            <li>About Us</li></Link>
+                        {location.pathname === '/' ?
+                            <Link to='about'
+                                spy={true}
+                                smooth={true}
+                                duration={700}
+                                className={style.navbar_item}>
+                                <li>About Us</li></Link> : <></>}
                         <NavLink className={({ isActive }) => (isActive ? style.navbar_item_active : style.navbar_item)} to='/menu'><li>Our Menu</li></NavLink>
                         <NavLink className={({ isActive }) => (isActive ? style.navbar_item_active : style.navbar_item)} to='/reservation'><li>Reservation</li></NavLink>
                         <NavLink className={({ isActive }) => (isActive ? style.navbar_item_active : style.navbar_item)} to='/blog'><li>Blog</li></NavLink>
